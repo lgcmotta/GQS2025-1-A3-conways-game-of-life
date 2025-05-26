a635fc0d9a838a4ed29cc4ab7522468e3d84ade8
Luiz Motta <lgcmotta01@gmail.com>
2025-05-25 20:00:40 -0700

    refactor: merge endpoint registration with delegating handlers (#20)

--- src/Conways.GameOfLife.API/Features/CreateBoard/CreateBoardEndpoint.cs
+++ src/Conways.GameOfLife.API/Features/CreateBoard/CreateBoardEndpoint.cs
@@ -1,11 +1,28 @@
+using Asp.Versioning;
+using Conways.GameOfLife.API.Diagnostics;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace Conways.GameOfLife.API.Features.CreateBoard;
 
 public static class CreateBoardEndpoint
 {
-    public static async Task<IResult> PostAsync(
+    public static RouteGroupBuilder MapCreateBoardEndpoint(this RouteGroupBuilder group, ApiVersion version)
+    {
+        group.MapPost("/boards", PostAsync)
+            .WithName("CreateNewBoard")
+            .WithDisplayName("Create New Board")
+            .WithTags("Create a New Board")
+            .MapToApiVersion(version)
+            .Produces<CreateBoardResponse>(contentType: MediaTypeNames.Application.Json)
+            .Produces<TracedProblemDetails>(StatusCodes.Status400BadRequest, MediaTypeNames.Application.ProblemJson)
+            .Produces<TracedProblemDetails>(StatusCodes.Status500InternalServerError, MediaTypeNames.Application.ProblemJson);
+
+        return group;
+    }
+
+    private static async Task<IResult> PostAsync(
         [FromBody] CreateBoardCommand command,
         [FromServices] IMediator mediator,
         CancellationToken cancellationToken = default)
--- src/Conways.GameOfLife.API/Features/NextGenerations/NextGenerationsEndpoint.cs
+++ src/Conways.GameOfLife.API/Features/NextGenerations/NextGenerationsEndpoint.cs
@@ -1,16 +1,33 @@
+using Asp.Versioning;
+using Conways.GameOfLife.API.Diagnostics;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace Conways.GameOfLife.API.Features.NextGenerations;
 
 public static class NextGenerationsEndpoint
 {
-    public static async Task<IResult> GetAsync(
+    public static RouteGroupBuilder MapNextGenerationsEndpoint(this RouteGroupBuilder group, ApiVersion version)
+    {
+        group.MapGet("/boards/{boardId}/generations/{generations:int}", GetAsync)
+            .WithName("GetNextGenerations")
+            .WithDisplayName("Get Board Next x Generations")
+            .WithTags("Get Board's Next x Generation")
+            .MapToApiVersion(version)
+            .Produces<NextGenerationsResponse>(contentType: MediaTypeNames.Application.Json)
+            .Produces<TracedProblemDetails>(StatusCodes.Status400BadRequest, MediaTypeNames.Application.ProblemJson)
+            .Produces<TracedProblemDetails>(StatusCodes.Status404NotFound, MediaTypeNames.Application.ProblemJson)
+            .Produces<TracedProblemDetails>(StatusCodes.Status500InternalServerError, MediaTypeNames.Application.ProblemJson);
+
+        return group;
+    }
+
+    private static async Task<IResult> GetAsync(
         string boardId,
         int generations,
         [FromServices] IMediator mediator,
-        CancellationToken cancellationToken = default
-    )
+        CancellationToken cancellationToken = default)
     {
         var response = await mediator.Send(new NextGenerationsQuery(boardId, generations), cancellationToken)
             .ConfigureAwait(continueOnCapturedContext: false);
