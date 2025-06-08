f96e535cc9089a1e1cec0fa4dbe0699ed9ea68f0
lgcmotta <lgcmotta01@gmail.com>
2025-06-08 19:10:42 -0300

    refactor: configure Scalar UI options

--- src/Conways.GameOfLife.API/Extensions/WebApplicationExtensions.cs
+++ src/Conways.GameOfLife.API/Extensions/WebApplicationExtensions.cs
@@ -1,10 +1,25 @@
 using Asp.Versioning;
 using Asp.Versioning.Builder;
+using Scalar.AspNetCore;
 
 namespace Conways.GameOfLife.API.Extensions;
 
 internal static class WebApplicationExtensions
 {
+    internal static WebApplication MapOpenApiUI(this WebApplication app)
+    {
+        app.MapScalarApiReference(options =>
+        {
+            options.WithTheme(ScalarTheme.DeepSpace)
+                .WithTitle("Conway's Game of Life")
+                .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient)
+                .WithDownloadButton(showDownloadButton: true)
+                .WithDotNetFlag(expose: true);
+        });
+
+        return app;
+    }
+
     internal static RouteGroupBuilder MapApiGroup(this WebApplication app, ApiVersion version)
     {
         var versionSet = app.BuildApiVersionSet(version);
